04327497a508539e5e6f6fcb7746550aa012ead4
Irinyi Kabinet Felhasználó <valaki@pcv6312-9.inf.u-szeged.hu>
2019-04-16 19:05:38 +0200

    route correction for json

--- routes.js
+++ routes.js
@@ -9,9 +9,9 @@ router.post('/login', function(req, res) {
         } else {
             req.logIn(username, function(error) {
                 if(error) {
-                    return res.status(500).send("Serialization error");
+                    return res.status(500).send({message: "Serialization error"});
                 } else {
-                    return res.status(200).send("Welcome");
+                    return res.status(200).send({message: "Welcome"});
                 }
             });
         }
@@ -21,18 +21,18 @@ router.post('/login', function(req, res) {
 router.post('/logout', function(req, res) {
     if(req.isAuthenticated()) {
         req.logout();
-        return res.status(200).send("logout successful");
+        return res.status(200).send({message: "logout successful"});
     }
-    return res.status(403).send("log in first");
+    return res.status(403).send({message: "log in first"});
     
 
 });
 
 router.get('/proba', function(req, res) {
     if(req.isAuthenticated()) {
-        return res.status(200).send("you are logged in");
+        return res.status(200).send({message: "you are logged in"});
     } else {
-        return res.status(403).send("you have no access");
+        return res.status(403).send({message: "you have no access"});
     }
 
 })
