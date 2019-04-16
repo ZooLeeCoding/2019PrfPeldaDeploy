81693e603115d6698d89c599d0fdb3056f7af1bd
Irinyi Kabinet Felhasználó <valaki@pcv6312-9.inf.u-szeged.hu>
2019-04-16 18:56:25 +0200

    correction

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000
 var app = express();
 
 // npm install cors --save
-//app.use(cors());
+app.use(cors());
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
@@ -45,7 +45,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 .set('views', path.join(__dirname, 'views'))
 .set('view engine', 'ejs')
 .get('/', (req, res) => res.render('pages/index'))
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 app.use('/', require('./routes'));
 
