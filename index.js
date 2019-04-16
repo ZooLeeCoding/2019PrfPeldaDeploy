af51d040df82d776655fc9dc40d59465ad7b1b2a
Irinyi Kabinet Felhasználó <valaki@pcv6312-9.inf.u-szeged.hu>
2019-04-16 18:49:51 +0200

    public folder

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ const PORT = process.env.PORT || 5000
 
 var app = express();
 
+// npm install cors --save
+app.use(cors());
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
@@ -37,6 +40,12 @@ app.use(expressSession({secret: '123456ezegyelrefijheigeihgieajhgijheaighiea'}))
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(express.static(path.join(__dirname, 'public')))
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'ejs')
+.get('/', (req, res) => res.render('pages/index'))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 app.use('/', require('./routes'));
 
